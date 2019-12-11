07faa667c9459d51c053ebc41a0f4945d9521a8e
1440407551 <1440407551@qq.com>
2019-12-11 19:23:25 +0800

    update

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,17 @@
-import Msite from '@/pages/Msite/Msite.vue'
-import Order from 'pages/Order/Order.vue'
-import Search from '../pages/Search/Search.vue'
-import Profile from '../pages/Profile/Profile.vue'
+// import Msite from '@/pages/Msite/Msite.vue'
+// import Order from 'pages/Order/Order.vue'
+// import Search from '../pages/Search/Search.vue'
+// import Profile from '../pages/Profile/Profile.vue'
+
+/**
+ * import(): 动态导入模块, 它会对被导入的模块进行单独打包 (code split)
+ * 定义的组件为一个返回promise的函数: 第一次请求路由对应的路径时会请求后台获取对应的路由打包文件
+ */
+const Msite = () => import('@/pages/Msite/Msite.vue')
+const Order = () => import('@/pages/Order/Order.vue')
+const Search = () => import('@/pages/Search/Search.vue')
+
+
 import Login from '../pages/Login/Login.vue'
 import Shop from '../pages/Shop/Shop.vue'
 import Goods from '../pages/Shop/Goods.vue'
@@ -41,7 +51,7 @@ export default [
     },
     {
         path: '/profile',
-        component: Profile,
+        component: () => import('@/pages/Profile/Profile.vue'),
         meta: {
             isShowFooter: true
         }
